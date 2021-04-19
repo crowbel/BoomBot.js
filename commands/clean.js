fea97ef8443cb317ba18ad4aea71920572f8ba10
crowbel <magkrstar@gmail.com>
2021-04-19 15:39:09 +0200

    Cleanup and improvments to clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -1,22 +1,24 @@
-    module.exports = {
-        name: 'clean',
-        descripion: "This command cleans all commands in chat" +
-            " and results of them to keep chat clean. " +
-            "Requires you specify how many messages you want deleted at the time. " +
-            "Will insult you if you fuck it up or don't have permissions.",
+module.exports = {
+    name: 'clean',
+    descripion: "This command cleans -bot commands in chat" +
+        "Requires you specify how many messages you want deleted at the time.",
 
-        execute(message, args){
-            if(!message.member.hasPermission("MANAGE_MESSAGES"))
-                return message.reply("Oyy, Zoomer! You Gen Z's don't have any rights here!").then(msg => msg.delete({timeout: 10000}));
+    async execute(message, args){
+        if(!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("Oyy, Zoomer! You Gen Z's don't have any rights here!").then(msg => msg.delete({timeout: 5000}));
 
-            if(!args[0])
-                return message.channel.send("Not how it works man! \n" +
-                    "Gotta specify how much you wanna delete like so: -clean 10").then(msg => msg.delete({timeout: 10000}));
+        if(!args[0])
+            return message.channel.send("Gotta specify how much you wanna delete like so: -clean 10").then(msg => msg.delete({timeout: 5000}));
 
-            message.channel.bulkDelete(args[0]).then()
-
-            message.channel.send(`Cleared out ${args[0]} messages.`).then(msg => msg.delete({timeout: 10000}));
+        if(args[0] < 1)
+            return message.reply("No point deleting 0 messages is it ?!").then(msg => msg.delete({timeout: 5000}));
 
 
-        }
+        await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages);
+        });
+
+        message.channel.send(`Cleared out ${args[0]} messages.`).then(msg => msg.delete({timeout: 5000}));
+
     }
+}
